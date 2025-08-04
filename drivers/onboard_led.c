5c7b9bb73254e9a18fb56e2f8591b2533441f3e4
Blair Leduc <BlairLeduc@users.noreply.github.com>
2025-08-04 14:03:01 -0400

    Upgrade to Pico SDK 2.2.0

--- drivers/onboard_led.c
+++ drivers/onboard_led.c
@@ -6,10 +6,7 @@
 //
 
 #include "pico/stdlib.h"
-
-#ifndef PICO_DEFAULT_LED_PIN
-#include "pico/cyw43_arch.h"
-#endif
+#include "pico/status_led.h"
 
 #include "onboard_led.h"
 
@@ -18,11 +15,7 @@ static bool led_initialised = false; // Flag to indicate if the LED is initializ
 // Set the state of the on-board LED
 void led_set(bool led)
 {
-#ifdef PICO_DEFAULT_LED_PIN
-    gpio_put(PICO_DEFAULT_LED_PIN, led ? 1 : 0);
-#else
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led ? 1 : 0);
-#endif
+    status_led_set_state(led);
 }
 
 // Initialize the LED driver
@@ -32,17 +25,9 @@ int led_init()
         return 0; // Already initialized, return success
     }
 
-#ifdef PICO_DEFAULT_LED_PIN
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    return 0; // Return 0 to indicate success
-#else
-    // If you have a wifi driver, call the initialiser function instead
-
-    // This will initialize the CYW43 Wi-Fi chip and its LED
-    return cyw43_arch_init();
-#endif
+    status_led_init();
 
     led_initialised = true; // Set the initialized flag
-}
 
+    return 0; // Success
+}
